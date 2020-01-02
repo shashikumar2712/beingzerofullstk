1b8fa4f98cdf5d15566da5552c3de0484b4816c1
shashikumar <shashipatel2712@gmail.com>
2020-01-02 17:27:31 +0530

    project success

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+ x:String ="random data";
   constructor() { }
 
   ngOnInit() {
+    this.x=this.x+"   new";
+  }
+  fnclick()
+  {
+    this.x=this.x+"   new";
   }
 
 }
